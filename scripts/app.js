e61599877492ca9811c0544e3f8573a4a2647afc
Two-Watch-Boy <twowatchboy@gmail.com>
2016-03-23 22:39:54 -0700

    working list of quakes and basic map

--- scripts/app.js
+++ scripts/app.js
@@ -4,7 +4,35 @@ var $quakesList;
 var map;
 var template;
 
-$(document).on("ready", function() {
+console.log("sanity check");
 
+// $(document).on("ready", function() {
+//    findQuakes();
+// });
 
-});
+$(document).on("ready", findQuakes);
+
+function findQuakes(){
+  console.log("hello");
+  $.ajax({
+    method: "GET",
+    url: weekly_quakes_endpoint,
+    // data: $("form").serialize(),
+    success: onSuccess,
+    error: onError
+  });
+}
+function onSuccess(json){
+  console.log(json);
+  // console.log(json.features[i].properties.place);
+  // console.log(json.features[i].geometry.coordinates);
+  for (var i=0;i<70;i++){
+    $("#quakes").append($("<li class='quake'>" + json.features[i].properties.place + " " + json.features[i].geometry.coordinates + "</li>"));
+  }
+  console.log("hi there");
+}
+function onError(xhs, status, errorThrown){
+  console.log(error);
+  }
+  // json.features[0].properties.title,
+  // json.features[0].properties.coordinates
